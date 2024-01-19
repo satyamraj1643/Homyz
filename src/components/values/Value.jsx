c85a72c52080641c84a13b3f52cd4f891bc61688
Satyam Raj <satyamraj1643@gmail.com>
2024-01-19 09:16:13 +0530

    changed

--- src/components/values/Value.jsx
+++ src/components/values/Value.jsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import Accordion from "@mui/material/Accordion";
-import AccordionActions from "@mui/material/AccordionActions";
+
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Button from "@mui/material/Button";
-import { FaBuildingCircleCheck } from "react-icons/fa6";
+
+
+
 import Downarrow from "../reusable/Downarrow";
 
 import "./Value.css";
 const Value = () => {
   const accordionStyle = {
-    boxShadow: "none",
+  
     border: "1px solid grey",
     boxShadow: "var(--shadow)",
   };
