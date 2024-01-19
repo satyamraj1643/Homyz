8dcff2eb55fdeeb91668a6eca411a0394abdbbd0
Satyam Raj <satyamraj1643@gmail.com>
2024-01-19 08:58:56 +0530

    production

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
